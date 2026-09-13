073a676c85c0137477158de4e53d9c4ac6eae4a4
Luan Tenorio <lluantenorio7@gmail.com>
2026-09-13 18:19:17 -0300

    mudando result

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -12,103 +12,103 @@ TEST_CASE("Vitória", "[velha]") {
   SECTION("Vitória do jogador X coluna") {
     GameMatrix game = {{2, 0, 1}, {2, 0, 1}, {0, 2, 1}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_X);
+    REQUIRE(CheckGame(game) == VICTORY_X);
   }
 
   SECTION("Vitória do jogador O coluna") {
     GameMatrix game = {{2, 0, 1}, {2, 0, 1}, {2, 1, 0}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_O);
+    REQUIRE(CheckGame(game) == VICTORY_O);
   }
 
   SECTION("Vitória do jogador X linha") {
     GameMatrix game = {{1, 1, 1}, {0, 0, 2}, {0, 2, 2}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_X);
+    REQUIRE(CheckGame(game) == VICTORY_X);
   }
 
   SECTION("Vitória do jogador O linha") {
     GameMatrix game = {{2, 2, 2}, {0, 0, 1}, {0, 1, 1}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_O);
+    REQUIRE(CheckGame(game) == VICTORY_O);
   }
 
   SECTION("Vitória do jogador X diagonal horária") {
     GameMatrix game = {{1, 0, 0}, {0, 1, 2}, {2, 2, 1}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_X);
+    REQUIRE(CheckGame(game) == VICTORY_X);
   }
 
   SECTION("Vitória do jogador X diagonal anti-horária") {
     GameMatrix game = {{0, 0, 1}, {0, 1, 2}, {1, 2, 2}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_X);
+    REQUIRE(CheckGame(game) == VICTORY_X);
   }
 
   SECTION("Vitória do jogador O diagonal horária") {
     GameMatrix game = {{2, 0, 0}, {0, 2, 1}, {1, 1, 2}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_O);
+    REQUIRE(CheckGame(game) == VICTORY_O);
   }
 
   SECTION("Vitória do jogador O diagonal anti-horário") {
     GameMatrix game = {{0, 0, 2}, {0, 2, 1}, {2, 1, 1}};
 
-    REQUIRE(VerificaVelha(game) == VICTORY_O);
+    REQUIRE(CheckGame(game) == VICTORY_O);
   }
 }
 
 TEST_CASE("Empate", "[velha]") {
   GameMatrix game = {{1, 2, 1}, {2, 1, 1}, {2, 1, 2}};
 
-  REQUIRE(VerificaVelha(game) == Result::DRAW);
+  REQUIRE(CheckGame(game) == Result::DRAW);
 }
 
 TEST_CASE("Impossível", "[velha]") {
   SECTION("Mais X que o permitido com o jogo completo") {
     GameMatrix game = {{1, 2, 1}, {2, 1, 1}, {1, 1, 2}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 
   SECTION("Mais O que o permitido com o jogo completo") {
     GameMatrix game = {{2, 1, 2}, {1, 2, 2}, {2, 2, 1}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 
   SECTION("Mais X que o permitido com o jogo incompleto") {
     GameMatrix game = {{1, 0, 1}, {2, 0, 1}, {1, 1, 2}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 
   SECTION("Mais O que o permitido com o jogo incompleto") {
     GameMatrix game = {{2, 0, 2}, {1, 0, 2}, {2, 2, 1}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 
   SECTION("Mais X que o permitido com o jogo no começo") {
     GameMatrix game = {{1, 0, 1}, {1, 0, 0}, {0, 2, 0}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 
   SECTION("Mais O que o permitido com o jogo no começo") {
     GameMatrix game = {{2, 0, 0}, {0, 0, 0}, {2, 0, 0}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 
   SECTION("2 Ganhadores") {
     GameMatrix game = {{2, 2, 2}, {1, 1, 1}, {0, 0, 0}};
 
-    REQUIRE(VerificaVelha(game) == Result::IMPOSSIBLE);
+    REQUIRE(CheckGame(game) == Result::IMPOSSIBLE);
   }
 }
 
 TEST_CASE("Indefinido", "[velha]") {
   GameMatrix game = {{1, 0, 0}, {0, 2, 0}, {1, 0, 2}};
-  REQUIRE(VerificaVelha(game) == Result::INDEFINITE);
+  REQUIRE(CheckGame(game) == Result::INDEFINITE);
 }
--- velha.hpp
+++ velha.hpp
@@ -6,8 +6,8 @@ enum Result {
   VICTORY_O = 2,
   VICTORY_X = 1,
   DRAW = 0,
-  IMPOSSIBLE = -1,
-  INDEFINITE = -2
+  INDEFINITE = -1,
+  IMPOSSIBLE = -2
 };
 
 enum Fields { FIELD_O = 2, FIELD_X = 1, Empty = 0 };
