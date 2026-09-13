31daac771ad332a2e1b392510b6a1befd8160219
Luan Tenorio <lluantenorio7@gmail.com>
2026-09-13 13:26:32 -0300

    Red: Vitória do jogador X diagonal anti-horário

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -33,8 +33,14 @@ TEST_CASE("Vitória", "[velha]") {
     REQUIRE(CheckGame(game) == VICTORY_O);
   }
 
-  SECTION("Vitória do jogador X diagonal horária") {
+  SECTION("Vitória do jogador X diagonal horário") {
     GameMatrix game = {{1, 0, 0}, {0, 1, 2}, {2, 2, 1}};
+
+    REQUIRE(CheckGame(game) == VICTORY_X);
+  }
+
+  SECTION("Vitória do jogador X diagonal anti-horário") {
+    GameMatrix game = {{0, 0, 1}, {0, 1, 2}, {1, 2, 2}};
 
     REQUIRE(CheckGame(game) == VICTORY_X);
   }
