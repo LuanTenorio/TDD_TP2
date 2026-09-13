43de5be4ff1d285c0d21c59228089b475e7e3cee
Luan Tenorio <lluantenorio7@gmail.com>
2026-09-12 21:15:21 -0300

    Red: Vitória do jogador X

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -8,7 +8,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("Testa velha", "[single-file]") {
-  int teste1[3][3] = {{2, 0, 1}, {2, 0, 1}, {0, 2, 1}};
-  REQUIRE(VerificaVelha(teste1) == 1);
+TEST_CASE("Vitória", "[velha]") {
+  SECTION("Vitória do jogador X") {
+    int game[3][3] = {{2, 0, 1}, {2, 0, 1}, {0, 2, 1}};
+
+    REQUIRE(VerificaVelha(game) == VICTORY_X);
+  }
 }
--- velha.hpp
+++ velha.hpp
@@ -2,6 +2,14 @@
 
 #define VELHA_HPP_
 
+enum Result {
+  VICTORY_O = 2,
+  VICTORY_X = 1,
+  DRAW = 0,
+  IMPOSSIBLE = -1,
+  INDEFINITE = -2
+};
+
 int VerificaVelha(int velha[3][3]);
 
 #endif  // VELHA_HPP_
